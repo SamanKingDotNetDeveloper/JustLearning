a55adb5ae8a72689fed88b267171f9023845d3f5
User <User@DESKTOP-R6A59AH>
2024-02-06 18:17:23 +0500

    Add project files.

---
+++ JustLearning/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace JustLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] arr = new int[a];
+            for(int i  = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            foreach(int x in arr) { Console.Write($"{x} "); }
+            Console.ReadKey();
+        }
+    }
+}
